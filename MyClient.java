5dac16fd99efc940722df6edc9647e6c9ad50c0a
h0tSauceKV <86956530+h0tSauceKV@users.noreply.github.com>
2022-05-29 23:44:12 +1000

    Update MyClient.java
    Stage 2 - New Scheduling Algorithm Development
    Based on Best Fit Algorithm.
    This modified algorithm also added new functionality to prioritized not only the servers with best fitness value, but also the ones with most running jobs that can fit other jobs with specified resource requirements.

--- MyClient.java
+++ MyClient.java
@@ -3,59 +3,53 @@ import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 
-public class MyClient {
+public class MyClient2 {
     public static void main(String[] args) {
         try {
             Socket s = new Socket("localhost", 50000);
             String msg = "";
-            String[] largestServer = { " " };
-
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            String[] bestFit = { " " };
 
             // hanshake protocol
-            dout.write(("HELO\n").getBytes());
+            sendMsg(s, "HELO\n");
             msg = readMsg(s);
-            System.out.println("Server says: " + msg);
-            dout.write(("AUTH " + System.getProperty("user.name") + "\n").getBytes());
+            System.out.println("RCVD: " + msg);
+            sendMsg(s, "AUTH " + System.getProperty("user.name") + "\n");
             msg = readMsg(s);
-            System.out.println("Server says: " + msg);
+            System.out.println("RCVD: " + msg);
 
             while (!msg.contains("NONE\n")) {
-                dout.write(("REDY\n").getBytes());
+                sendMsg(s, "REDY\n");
                 msg = readMsg(s);
 
                 if (msg.contains("JOBN")) {
-                    // Identify the largest server type
                     String[] Jobs = msg.split(" ");
-                    dout.write(("GETS Avail " + Jobs[4] + " " + Jobs[5] + " " + Jobs[6] + "\n").getBytes());
+                    sendMsg(s, "GETS Avail " + Jobs[4] + " " + Jobs[5] + " " + Jobs[6] + "\n");
                     // Receive DATA nRecs recSize
                     msg = readMsg(s);
-                    dout.write(("OK\n").getBytes());
+                    sendMsg(s, "OK\n");
 
                     msg = readMsg(s);
-                    dout.write(("OK\n").getBytes());
+                    sendMsg(s, "OK\n");
 
-                    // Keep track of the largest server type and the number of servers of that type
-                    largestServer = findLargestServer(msg);
+                    // Request for the best fitted server
+                    bestFit = bestFit(msg, Jobs[4], s);
 
                     // Reads "." from the server
                     msg = readMsg(s);
 
-                    // Schedule a job
-                    dout.write(("SCHD " + Jobs[2] + " " + largestServer[0] + " " + largestServer[1] + "\n").getBytes());
-
+                    // Schedule a job to an available server that can eventually provide sufficient
+                    // resource
+                    sendMsg(s, "SCHD " + Jobs[2] + " " + bestFit[0] + " " + bestFit[1] + "\n");
                     msg = readMsg(s);
                     System.out.println("SCHD: " + msg);
 
                 } else if (msg.contains("DATA")) {
-                    dout.write(("OK\n").getBytes());
+                    sendMsg(s, "OK\n");
                 }
             }
-
-            dout.write(("QUIT\n").getBytes());
+            sendMsg(s, "QUIT\n");
             msg = readMsg(s);
-
-            dout.flush();
             s.close();
 
         } catch (Exception e) {
@@ -67,7 +61,7 @@ public class MyClient {
         String msg = "";
         try {
             DataInputStream dis = new DataInputStream(s.getInputStream());
-            // Create an array of bytes and reset it to 0 for storing
+            // Create byte array and reset it to 0 for storing
             // incoming messages from the server
             byte[] byteArray = new byte[dis.available()];
             // available() method blocks until input data is available
@@ -78,34 +72,85 @@ public class MyClient {
                 dis.read(byteArray);
                 msg = new String(byteArray, StandardCharsets.UTF_8);
             }
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        // Return the msg just recieved from the server
         return msg;
     }
 
-    public static String[] findLargestServer(String msg) {
-        // Servers in the msg spilt into an array
-        String[] serverRecs = msg.split("\n");
-        int mostCores = 0;
-        String[] currServer = { " " };
-        // Search for the most core a server holds in the given server
-        for (int i = 0; i < serverRecs.length; i++) {
-            currServer = serverRecs[i].split(" ");
-            int currCores = Integer.valueOf(currServer[4]);
-            if (currCores > mostCores) {
-                mostCores = currCores;
-            }
+    public static void sendMsg(Socket s, String msg) {
+        try {
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            byte[] byteArray = msg.getBytes();
+            dout.write(byteArray);
+            dout.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        // search the biggest server (most cores)
-        for (int i = 0; i < serverRecs.length; i++) {
-            currServer = serverRecs[i].split(" ");
-            int currCores = Integer.valueOf(currServer[4]);
-            if (currCores == mostCores) {
-                return currServer;
+    public static String[] bestFit(String msg, String jobCores, Socket s) {
+        // Servers in the msg spilt into an array
+        String[] servers = msg.split("\n");
+        String[] jobRunningServer = { "" };
+        String[] bestFitServer = servers[0].split("");
+        int currentCores = Integer.parseInt(bestFitServer[4]);
+        int coresRemain = currentCores - Integer.parseInt(jobCores);
+        int fitnessValue = coresRemain;
+        int jobCount;
+        int mostJobs = -1;
+        int jobRunningServerFitnessValue = -1;
+
+        // Traverse through the list of available servers to find best fit
+        for (int i = 0; i < servers.length; i++) {
+            // null check on servers list
+            if (!servers[i].contains(".")) {
+                // Picking the server with the best fitness value (lowest) to be compared
+                String[] currentServer = servers[i].split(" ");
+                currentCores = Integer.parseInt(currentServer[4]);
+                coresRemain = currentCores - Integer.parseInt(jobCores);
+
+                // Request the current jobs running on the server
+                String currMsg = "CNTJ " + currentServer[0] + " " + currentServer[1] + " " + 2 + "\n";
+                sendMsg(s, currMsg);
+
+                // Save the jobs to be compared
+                String currentJobs = readMsg(s);
+
+                String[] jobs = currentJobs.split("");
+                if (jobs.length == 2) {
+                    jobCount = Integer.parseInt(jobs[1]);
+                } else {
+                    jobCount = Integer.parseInt(jobs[0]);
+                }
+                // Picking the server with the most running jobs
+                if (jobCount > 0) {
+                    if (jobCount > mostJobs) {
+                        mostJobs = jobCount;
+                        fitnessValue = coresRemain;
+                        currentServer = bestFitServer;
+                    }
+                }
             }
         }
-        return currentServer;
+        // high prioritise on finding server with jobs running
+        // server with the lowest fitness value is prioritised
+        // if fv = 0, return the current server to be best fitted
+        if (bestFitServer.length > 1) {
+            if (coresRemain == 0) {
+                return bestFitServer;
+            } else {
+                jobRunningServer = bestFitServer;
+                jobRunningServerFitnessValue = fitnessValue;
+            }
+        } else if (coresRemain != 0 || coresRemain < fitnessValue) {
+            if (coresRemain < jobRunningServerFitnessValue) {
+                return bestFitServer;
+            } else {
+                return jobRunningServer;
+            }
+        }
+        return bestFitServer;
     }
 }
